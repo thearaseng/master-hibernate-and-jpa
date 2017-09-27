8f48df191fc2770b061d97f59e8e4ed8fafabc34
Theara Seng <seng.theara.kh@gmail.com>
2017-09-27 09:56:06 +0700

    initial commit

---
+++ src/main/java/com/example/masterhibernateandjpa/MasterHibernateAndJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.masterhibernateandjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterHibernateAndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterHibernateAndJpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/masterhibernateandjpa/MasterHibernateAndJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.masterhibernateandjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasterHibernateAndJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
