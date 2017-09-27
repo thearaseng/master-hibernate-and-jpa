90368f7c61458d614190e977fc4812e255628b8c
Theara Seng <seng.theara.kh@gmail.com>
2017-09-27 10:21:59 +0700

    add some data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,10 @@ CREATE TABLE person(
   PRIMARY KEY(id)
 
 );
+
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10001,  'Ranga', 'Hyderabad',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10002,  'James', 'New York',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10003,  'Pieter', 'Amsterdam',sysdate());
