f74c104eadbfe05f0038e3d7216ceebef83cd38a
Theara Seng <seng.theara.kh@gmail.com>
2017-10-04 13:50:16 +0700

    add Review entity and some sample data

---
+++ src/main/java/com/example/masterhibernateandjpa/entity/Review.java
@@ -0,0 +1,55 @@
+package com.example.masterhibernateandjpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String rating;
+
+    private String description;
+
+    public Review(){}
+
+    public Review(String rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", rating='" + rating + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,6 +19,14 @@ INSERT INTO course(id, name, created_date, last_updated_date) VALUES (10001, 'Ja
 INSERT INTO course(id, name, created_date, last_updated_date) VALUES (10002, 'Php Developer', sysdate(), sysdate());
 INSERT INTO course(id, name, created_date, last_updated_date) VALUES (10003, 'Ruby Developer', sysdate(), sysdate());
 
-INSERT INTO student(id, number) VALUES (20001, 'Theara');
-INSERT INTO student(id, number) VALUES (20001, 'Dara');
-INSERT INTO student(id, number) VALUES (20001, 'Rotha');
+INSERT INTO student(id, name) VALUES (20001, 'Theara');
+INSERT INTO student(id, name) VALUES (20002, 'Dara');
+INSERT INTO student(id, name) VALUES (20003, 'Rotha');
+
+INSERT INTO passport(id, number) VALUES (40001, 'E48487554');
+INSERT INTO passport(id, number) VALUES (40002, 'E63859954');
+INSERT INTO passport(id, number) VALUES (40003, 'E94938858');
+
+INSERT INTO review(id, rating, description) VALUES (50001, '5', 'Description 1');
+INSERT INTO review(id, rating, description) VALUES (50002, '2', 'Description 2');
+INSERT INTO review(id, rating, description) VALUES (50003, '4', 'Description 3');
